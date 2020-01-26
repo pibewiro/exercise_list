d2bc807c0e3217b371c89d5582dfada1fcbfcdfa
prince <pibewiro@yahoo.com>
2020-01-26 14:35:47 -0300

    heroku

---
+++ models/exercise.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    username:{type:String, required:true},
+    description:{type:String, required:true},
+    date:{type:Date, required:true},
+    duration:{type:Number, required:true}
+}, {timestamps:true})
+
+const exercise = mongoose.model("exercise", exerciseSchema);
+module.exports = exercise;
