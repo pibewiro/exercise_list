b63a9849cce0f84be1c2deb44fa5f2724491e9b7
prince <pibewiro@yahoo.com>
2020-01-26 15:37:11 -0300

    update

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const User = require("../models/user.model")
 router.route("/").get((req,res)=>{
     User.find()
     .then(user=>{
-        res.json(user)
+        return res.status(200).json(user)
     })
     .catch(err=>res.status(400).json("Error: ", err))
 })
